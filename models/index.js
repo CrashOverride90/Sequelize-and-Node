691610f7e77d28585855d4a690e1cec5e474ae53
Ashik Khatri <ashikrk@gmail.com>
2022-11-30 14:14:33 -0700

    remove console logs

--- models/index.js
+++ models/index.js
@@ -22,10 +22,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    console.log(file);
-    console.log(path.join(__dirname, file));
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    console.log(model);
     db[model.name] = model;
   });
 
