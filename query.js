d28b671cdd049c41b87b2e597a83d2471f743718
Ashik Khatri <ashikrk@gmail.com>
2022-12-01 19:20:09 -0700

    update

--- query.js
+++ query.js
@@ -1,9 +1,4 @@
 const { User, Task } = require('./models')
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op
-
-// Find all users with their associated tasks
-// Raw SQL: SELECT * FROM "Users" JOIN "Tasks" ON "Tasks"."userId" = "Users".id;
 
 const findAllWithTasks = async () => {
     const users = await User.findAll({
